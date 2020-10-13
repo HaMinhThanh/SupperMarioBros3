2dd6af7586ab10b9396284e5041e5e6143299bfa
HaMinhThanh <71711582+HaMinhThanh@users.noreply.github.com>
2020-10-13 18:11:09 +0700

    Update

---
+++ SupperMario/SupperMario/Brick.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "GameObject.h"
+
+#define BRICK_BBOX_WIDTH 16
+#define BRICK_BBOX_HEIGHT 16
+
+class Brick: public GameObject
+{
+public:
+	virtual void Render();
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+
+};
+
